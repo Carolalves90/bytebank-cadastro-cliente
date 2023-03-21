2eab3b310d07ec6ebb80011bd92966f4da1c2927
Carolalves90 <carolalves.tech@gmail.com>
2023-03-20 21:46:42 -0300

    :sparkles: adicionando conta poupança

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import { Cliente } from "./cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 //cliente1 é instancia da classe Cliente
-const cliente1 = new Cliente("Ricardo",11122233309);
+const cliente1 = new Cliente("Ricardo", 11122233309);
 //cada cliente é um objeto
-const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const conta1= new ContaCorrente(1001, cliente1);
-conta1.depositar(500);
-const conta2 = new ContaCorrente(1002, cliente2);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-conta1.transferir(200, conta2);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-console.log(conta1);
-console.log(cliente2);
-console.log(ContaCorrente.numeroDeContas)
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
