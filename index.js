fea3f2fe8701c107d9d5fcf9077527ff3003753c
Carolalves90 <carolalves.tech@gmail.com>
2023-03-24 19:59:34 -0300

    :sparkles: cadastro e senha de funcionarios

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import { Cliente } from "./cliente.js";
-import { Conta } from "./Conta.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-//cliente1 é instancia da classe Cliente
-const cliente1 = new Cliente("Ricardo", 11122233309);
-//cada cliente é um objeto
+const diretor = new Diretor("Rodrigo", 10000, 12345678900)
+diretor.cadastrarSenha("123456789")
+const gerente = new Gerente("Ricardo", 5000, 12378945601)
+gerente.cadastrarSenha("12378945601")
 
+const estaLogado = SistemaAutenticacao.login(gerente, "12378945601");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario)
+console.log(estaLogado)
